43cd5a371e398305a15d9e6d4ae83734ad92410a
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-09-10 22:23:37 +0100

    edited

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int minLength = 7;
+string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string lowercase = "abcdefghijklmnopqrstuvwxyz";
+string digits = "1234567890";
+string specialChars = "!@#$%^&*()-_+=\\/':,{}[]~.";
+
+Console.WriteLine("Please enter a password");
+string password = Console.ReadLine();
+
+int score = 0;
+
+if (password.Length >= minLength)
+{
+  score ++;
+}
+if (Tools.Contains(password, uppercase))
+{
+  score++;
+}
+if (Tools.Contains(password, lowercase))
+{
+  score++;
+}
+if(Tools.Contains(password, digits))
+{
+  score++;
+}
+if(Tools.Contains(password, specialChars))
+{
+  score++;
+}
+Console.WriteLine(score);
